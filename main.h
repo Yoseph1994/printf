a2658ad5c4227e5447bfdbec6d53ae19a7247d7b
Yoseph Shimelis <140092711+Yoseph1994@users.noreply.github.com>
2023-10-18 19:01:11 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,10 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdarg.h>
-#include <string.h>
-#include <unistd.h>
 
 int printf_char(va_list val);
 int printf_string(va_list val);
