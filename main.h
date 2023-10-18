7f390deb1ef18e19166d08c53825e73e76338954
Yoseph1994 <joshrde2002@gmail.com>
2023-10-18 08:44:13 -0700

    header file fix

--- main.h
+++ main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+
 int printf_char(va_list val);
 int printf_string(va_list val);
 int _strlen(char *s);
