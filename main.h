ef34a3b10088c0e3d5a5e132f5e8cc918ec0b48f
Yoseph Shimelis <140092711+Yoseph1994@users.noreply.github.com>
2023-10-18 18:01:07 +0300

    added prototype for % printf in main.h

--- main.h
+++ main.h
@@ -5,6 +5,7 @@ int printf_char(va_list val);
 int printf_string(va_list val);
 int _strlen(char *s);
 int _strlenc(const char *s);
+int printf_37(void);
 int _putchar(char c);
 int _printf(const char *format,...);
 
